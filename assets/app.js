01c4ec9e51bb3e4ea0736d75847bdb7e8309d131
tonytillet <tony.tillet@gmail.com>
2024-08-16 00:01:29 +0200

    MASTER : review webpack

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,19 @@
 import './bootstrap.js';
+import './main.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
  */
 
-
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
 import './styles/vendor/aos/aos.css';
+import './styles/vendor/aos/aos.js';
 import './styles/vendor/bootstrap/css/bootstrap.min.css';
-import './styles/vendor/bootstrap-icons/bootstrap-icons.css';
-import './styles/vendor/glightbox/css/glightbox.min.css';
-import './styles/vendor/remixicon/remixicon.css';
+import './styles/vendor/glightbox/css/glightbox.css';
 import './styles/vendor/swiper/swiper-bundle.min.css';
-import './styles/style.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+import './styles/style.css';
